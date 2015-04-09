0df7b5c1b7cf2f21cabdb626722a474634c14632
Egga (Jan Hartung) <github@egga.de>
2015-04-09 21:36:18 +0200

    add java/assert_j

---
+++ java/assert_j/src/test/java/ThingTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThingTest {
+
+    @Test
+    public void it_should_call_for_action() {
+        Thing thing = new Thing();
+        String value = thing.callForAction();
+        assertThat(value).isEqualTo("Food");
+    }
+}
