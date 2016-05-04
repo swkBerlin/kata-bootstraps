6d1ed9f4415c2024f11a9f76f332c9b29231f105
Markus Decke <mbde@users.noreply.github.com>
2016-05-04 19:02:00 +0200

    adds java bootstrap for maven
    includes: junit, assertj and mockito

---
+++ java/maven-junit/src/main/java/net/example/Example.java
@@ -0,0 +1,4 @@
+package net.example;
+
+public class Example {
+}
---
+++ java/maven-junit/src/test/java/net/example/ExampleTest.java
@@ -0,0 +1,14 @@
+package net.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void test(){
+        fail("starting point for TDD cycle");
+    }
+
+}
