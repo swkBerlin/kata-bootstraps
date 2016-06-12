70e66b33afb73d09af77eec12e65eda8168e58d4
Michele Adduci <adduci.michele@gmail.com>
2016-06-12 12:24:56 +0200

    Refactored to use a generic kata test and added explicit compiler flag to enable C++11

--- cpp/src/kata.cpp
+++ cpp/src/kata.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "doctest.h"
 
-TEST_CASE("Testing Hello World")
+TEST_CASE("Testing Kata Hello World")
 {
     constexpr auto message{"Hello World"};
     CHECK(message == "Hello World");
