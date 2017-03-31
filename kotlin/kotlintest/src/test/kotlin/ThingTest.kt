4d7e674c6407955c10bc000412b194a3748636e1
Christof Vollrath <christof@taobits.net>
2017-04-01 00:05:43 +0200

    New kotlin/kotlintest project

---
+++ kotlin/kotlintest/src/test/kotlin/ThingTest.kt
@@ -0,0 +1,29 @@
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.properties.forAll
+import io.kotlintest.properties.headers
+import io.kotlintest.properties.row
+import io.kotlintest.properties.table
+import io.kotlintest.specs.StringSpec
+
+class ThingTest : StringSpec({
+    "it should callForAction" {
+        val thing = Thing()
+        val value = thing.callForAction()
+        value shouldBe "Food"
+    }
+
+    "table callForAction" {
+        val thing = Thing()
+
+        val testTable = table(
+            headers("food",      "result"),
+            //-----|-----------|------------------------
+            row    ("chocolate", "Dog eats chocolate."),
+            row    ("bone",      "Dog eats fish.")
+        )
+        forAll(testTable) { food, result ->
+            thing.callForAction(food) shouldBe result
+        }
+    }
+
+})
