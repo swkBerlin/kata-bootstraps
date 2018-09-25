a98a351ae3795f95dc28934df31d7227483c3bf7
Charles Ahmadzadeh <superzadeh@outlook.com>
2018-09-25 10:20:02 +0200

    Add a TypeScript & Jest boilerplate 🎉

---
+++ ts/typescript-jest/src/HelloWorld.test.ts
@@ -0,0 +1,38 @@
+import { HelloWorld } from './HelloWorld'
+import { RemoteLogger } from './RemoteLogger'
+
+// 👇 Test example using a mock/spy without async/await
+describe('Saying Hello World', () => {
+  it('should log something using the remote logger', () => {
+    // Arrange
+    const message: string = 'Hello World!'
+    const stubLogger: RemoteLogger = {
+      Log: jest.fn()
+    }
+    const sut = new HelloWorld(stubLogger)
+
+    // Act
+    sut.Say(message)
+
+    // Assert
+    expect(stubLogger.Log).toHaveBeenCalledWith(message)
+  })
+})
+
+// 👇 Test example using a mock/spy with async/await
+describe('Saying Hello World asynchronously', () => {
+  it('should log something using the remote logger', async () => {
+    // Arrange
+    const message: string = 'Hello World!'
+    const stubLogger: RemoteLogger = {
+      Log: jest.fn()
+    }
+    const sut = new HelloWorld(stubLogger)
+
+    // Act
+    await sut.SayAsync(message)
+
+    // Assert
+    expect(stubLogger.Log).toHaveBeenCalledWith(message)
+  })
+})
