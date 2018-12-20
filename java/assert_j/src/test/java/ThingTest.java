9507d3b6891571e690ede8cf0eca90b57249ce0d
Lars Eckart <lars.eckart@gmail.com>
2018-12-20 23:14:59 +0200

    assertj project, make test package private
    afaik that is the convention

--- java/assert_j/src/test/java/ThingTest.java
+++ java/assert_j/src/test/java/ThingTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ThingTest {
 
     @Test
-    public void it_should_call_for_action() {
+    void it_should_call_for_action() {
         Thing thing = new Thing();
         String value = thing.callForAction();
         assertThat(value).isEqualTo("Food");
