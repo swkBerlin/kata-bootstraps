614d407f4d34f8b39066eb5fa5ce568e255098e0
Martin Klose <martin.klose@klosebrothers.de>
2019-12-13 11:30:22 +0100

    add other projects to build

--- java/spock/src/test/groovy/ThingSpec.groovy
+++ java/spock/src/test/groovy/ThingSpec.groovy
@@ -1,6 +1,6 @@
 import spock.lang.Specification
 
-class ThingSpec extends Specification {
+  class ThingSpec extends Specification {
 
   def "it should call for action"() {
     given: "some Thing"
@@ -11,5 +11,16 @@ class ThingSpec extends Specification {
 
     then: "it should return 'Food'"
     "Food" == value
+  }
+
+  def notfail() {
+    given: "1"
+    def value = 1
+
+    when: "I add 1 to it"
+    value +=1
+
+    then: "it should be 2"
+    2 == value
   }
 }
