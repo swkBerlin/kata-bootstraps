3f3edfcfbc1b482ab05c31ad3473a4ec4a50b338
dmitry.kruglov <kruglov.dima@gmail.com>
2020-10-23 16:44:24 +0200

    test double WIP

--- python/pytest/test_double.py
+++ python/pytest/test_double.py
@@ -1,3 +1,6 @@
+import mock
+
+
 class SomeService:
     def service_call(self, number):
         # going into db looong complex processing
@@ -9,7 +12,6 @@ def under_test(service, number):
 
 
 class ServiceStub:
-
     def service_call(self, number):
         return "42"
 
@@ -22,3 +24,23 @@ def test_stub():
 def test_mock():
     result = under_test(ServiceStub(), 0)
     assert result == "42"
+
+
+def slow_function():
+    return 123
+
+
+@mock.patch('slow_function')
+def test_expectation():
+    mock.patch(
+        # api_call is from slow.py but imported to main.py
+        'slow_function',
+        return_value=0
+    )
+
+
+    #
+    expected = 0
+    actual = slow_function()
+    assert expected == actual
+
