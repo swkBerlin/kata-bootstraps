9492dd73a71098c38f5f29f34a29ae28332570ee
Alexander Kosik <alexander.kosik@aareal-bank.com>
2020-11-07 10:17:21 +0100

    Parametrized python tests
    needs parametrized module

--- python/unittest/todo_project_name_test.py
+++ python/unittest/todo_project_name_test.py
@@ -1,7 +1,35 @@
 import unittest
+from parameterized import parameterized
 
+def mutation(alive_neighbours, current_cell_state):
+    return current_cell_state and alive_neighbours == 2 or alive_neighbours == 3
 
 class TodoProjectNameTestCase(unittest.TestCase):
+    @parameterized.expand([
+        ["underpopulation_1", 1, False],
+        ["underpopulation_smaller_1", 0, False],
+        ["overcrowding", 4, False],
+        ["overcrowding_bigger_4", 5, False],
+        ["stay_alive", 2, True],
+        ["stay_alive_3", 3, True]
+    ])
+    def test_mutation_of_alive_cell(self, name, alive_neighbours, cell_is_alive):
+        cell_state_after_mutation = mutation(alive_neighbours, True)
+        self.assertEqual(cell_state_after_mutation, cell_is_alive)
 
-    def test_python_test_setup_works(self):
-        self.assertEqual(42, 43)
+
+    @parameterized.expand([
+        ["underpopulation_1", 1, False],
+        ["underpopulation_smaller_1", 0, False],
+        ["overcrowding", 4, False],
+        ["overcrowding_bigger", 5, False],
+        ["stay_dead", 2, False],
+        ["resurrection", 3, True]
+    ])
+    def test_mutation_of_dead_cell(self, name, alive_neighbours, cell_is_alive):
+        cell_state_after_mutation = mutation(alive_neighbours, False)
+        self.assertEqual(cell_state_after_mutation, cell_is_alive)
+
+
+if __name__ == '__main__':
+    unittest.main()
