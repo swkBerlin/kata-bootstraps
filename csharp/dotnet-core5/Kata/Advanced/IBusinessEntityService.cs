b97f035a42effc49f10aa78ff9fa514c24d2596a
Patrick Drechsler <patrick@pdrechsler.de>
2020-12-16 11:04:22 +0100

    csharp: add dotnet-core 5 template

---
+++ csharp/dotnet-core5/Kata/Advanced/IBusinessEntityService.cs
@@ -0,0 +1,27 @@
+namespace Kata.Advanced
+{
+    public interface IBusinessEntityService
+    {
+        int Store(BusinessEntity entity);
+        BusinessEntity Get(int id);
+    }
+    public class BusinessEntityService : IBusinessEntityService
+    {
+        private IRepository<BusinessEntity> _repo;
+
+        public BusinessEntityService(IRepository<BusinessEntity> repo)
+        {
+            _repo = repo;
+        }
+
+        public BusinessEntity Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public int Store(BusinessEntity entity)
+        {
+            return _repo.Add(entity);
+        }
+    }
+}
