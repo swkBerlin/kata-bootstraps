584c698b29bf4610eb29d6d7f9c53e234ea63d96
Martin Klose <martin.klose@klosebrothers.de>
2021-01-22 23:20:56 +0100

    move scripts

--- java/test_gradlew.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find . -type f -name build.gradle | xargs -I {} sh -c "dirname {}" | xargs -I {} sh -c "cd {} && pwd && ./gradlew clean test --warning-mode all"
--- java/update_gradlew.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-GRADLE_VERSION="6.1.1"
-find . -type f -name build.gradle | xargs -I {} sh -c "dirname {}" | xargs -I {} sh -c "cd {} && pwd && gradle wrapper --gradle-version ${GRADLE_VERSION}"
