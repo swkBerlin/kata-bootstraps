845aeb58c289d2c6f7ad0fe6440205c016013fef
Martin Klose <martin.klose@klosebrothers.de>
2021-09-28 16:27:00 +0200

    update gradle to 7.2 and fix some issues

--- java/.scripts/update_gradlew.sh
+++ java/.scripts/update_gradlew.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-GRADLE_VERSION="6.8"
-find ../. -type f -name build.gradle | xargs -I {} sh -c "dirname {}" | xargs -I {} sh -c "cd {} && pwd && gradle wrapper --gradle-version ${GRADLE_VERSION}"
+GRADLE_VERSION="7.2"
+find ../. -maxdepth 2 -type f -name build.gradle | xargs -I {} sh -c "dirname {}" | xargs -I {} sh -c "cd {} && pwd && gradle wrapper --gradle-version ${GRADLE_VERSION}"
