eba52648a5b23a661f071620ed7cc5b3abeb6f88
yak <yak.hawkes@interhome.group>
2021-10-22 16:55:49 +0200

    Much test refactoring

--- csharp/dotnet-core5/Kata.Tests/Tests.cs
+++ csharp/dotnet-core5/Kata.Tests/Tests.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using Moq;
 using Xunit;
 using FluentAssertions;
@@ -10,6 +12,12 @@ namespace Kata.Tests
     private readonly Mock<IDisplay> _displayMock;
     private readonly GameScore _gameScore;
 
+    private enum Player
+    {
+      One = 1,
+      Two = 2
+    };
+
     public Tests()
     {
       _displayMock = new Mock<IDisplay>();
@@ -19,14 +27,64 @@ namespace Kata.Tests
     [Fact]
     public void InitializeGameScore_ShouldBeLove()
     {
-      _displayMock.Verify(x => x.DisplayScore("Love - Love"), Times.Once);
+      _displayMock.Verify(x => x.DisplayScore($"{GameScore.Love} - {GameScore.Love}"), Times.Once);
     }
 
     [Fact]
     public void PlayerScores_ShouldCallDisplayScoreTwice()
     {
-      _gameScore.PlayerScores();
+      SimulateGame(Player.One);
       _displayMock.Verify(x => x.DisplayScore(It.IsAny<string>()), Times.Exactly(2));
+    }
+
+    [Fact]
+    public void PlayerOneScoresOnce_ShouldBe15Love()
+    {
+      SimulateGame(Player.One);
+      _displayMock.Verify(x => x.DisplayScore($"{GameScore.Fifteen} - {GameScore.Love}"), Times.Once);
+    }
+
+    [Fact]
+    public void PlayerOneScoresTwice_ShouldBe30Love()
+    {
+      SimulateGame(Player.One, Player.One);
+      _displayMock.Verify(x => x.DisplayScore($"{GameScore.Thirty} - {GameScore.Love}"), Times.Once);
+    }
+
+    [Fact]
+    public void PlayerTwoScoresOnce_ShouldBeLove15()
+    {
+      SimulateGame(Player.Two);
+      _displayMock.Verify(x => x.DisplayScore($"{GameScore.Love} - {GameScore.Fifteen}"), Times.Once);
+    }
+
+    [Fact]
+    public void PlayerTwoScoresTwice_ShouldBeLove30()
+    {
+      SimulateGame(Player.Two, Player.Two);
+      _displayMock.Verify(x => x.DisplayScore($"{GameScore.Love} - {GameScore.Thirty}"), Times.Once);
+    }
+
+    [Fact]
+    public void PlayerOneScoresOnce_PlayerTwoScoresOnce_ShouldBeLove30()
+    {
+      SimulateGame(Player.One, Player.Two);
+      _displayMock.Verify(x => x.DisplayScore($"{GameScore.Fifteen} - {GameScore.Fifteen}"), Times.Once);
+    }
+    
+    private void SimulateGame(params Player[] playerScores)
+    {
+      foreach (Player player in playerScores)
+      {
+        if (player == Player.One)
+        {
+          _gameScore.PlayerOneScores();
+        }
+        else if( player == Player.Two)
+        {
+          _gameScore.PlayerTwoScores();
+        }
+      }
     }
   }
 }
--- csharp/dotnet-core5/Kata/GameScore.cs
+++ csharp/dotnet-core5/Kata/GameScore.cs
@@ -1,16 +1,44 @@
-﻿namespace Kata
+﻿using System.Collections.Generic;
+
+namespace Kata
 {
   internal class GameScore
   {
+    internal const string Love = "Love";
+    internal const string Fifteen = "15";
+    internal const string Thirty = "30";
     private readonly IDisplay _display;
+    private readonly List<string> scoreOrder = new ()
+    {
+      Love, Fifteen, Thirty
+    };
+    private int countPlayerOne;
+    private int countPlayerTwo;
     internal GameScore(IDisplay display)
     {
       _display = display;
-      _display.DisplayScore("Love - Love");
+      countPlayerOne = 0;
+      countPlayerTwo = 0;
+      BeautifyScore();
     }
-    internal void PlayerScores()
+
+    internal void PlayerOneScores()
     {
-      _display.DisplayScore("");
+      countPlayerOne++;
+      BeautifyScore();
     }
+
+    internal void PlayerTwoScores()
+    {
+      countPlayerTwo++;
+      BeautifyScore();
+    }
+
+    private void BeautifyScore()
+    {
+      _display.DisplayScore($"{scoreOrder[countPlayerOne]} - {scoreOrder[countPlayerTwo]}");
+    }
+
+
   }
 }
