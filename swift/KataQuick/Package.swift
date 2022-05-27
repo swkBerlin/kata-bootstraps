1ddcae555b51109c84f2ef1b7db9530f65f23926
Dan Shevluk <dan.shevluk@hey.com>
2022-05-27 18:01:59 +0300

    Add Swift Kata project with Quick + Nimble

---
+++ swift/KataQuick/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "KataQuick",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Kata",
+            targets: ["Kata"]),
+    ],
+    dependencies: [
+         .package(url: "https://github.com/Quick/Quick.git", from: "5.0.1"),
+         .package(url: "https://github.com/Quick/Nimble.git", from: "9.1.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Kata",
+            dependencies: []),
+        .testTarget(
+            name: "KataTests",
+             dependencies: ["Kata", "Quick", "Nimble"]),
+    ]
+)
