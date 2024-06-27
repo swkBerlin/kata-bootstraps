203d7afbda534ae6f30771cc8a46864e3e50291b
Maros Sklenica <m.sklenica.external@sportradar.com>
2024-06-27 07:44:49 +0200

    Testing push

--- php/tests/KataTest.php
+++ php/tests/KataTest.php
@@ -11,16 +11,8 @@ class KataTest extends TestCase
 {
     public function testDummy(): void
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
         $kata = new Kata();
         $this->assertInstanceOf(Kata::class, $kata);
-=======
-        $kat = new Kata();
->>>>>>> 06f01f1016fb99e97882a042564c4b7539b6b3cd
-=======
-        $kat = new Kata();
->>>>>>> 06f01f1016fb99e97882a042564c4b7539b6b3cd
         $this->assertTrue(false);
     }
 
