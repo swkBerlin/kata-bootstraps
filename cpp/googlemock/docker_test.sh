db4835ae37b93859c5f28fcabb9ddbae49cafbb9
Martin Klose <109064+mklose@users.noreply.github.com>
2026-03-10 13:42:28 +0100

    clean up

--- cpp/googlemock/docker_test.sh
+++ cpp/googlemock/docker_test.sh
@@ -4,4 +4,4 @@ docker run \
   -v $(pwd):/kata \
   -w /kata \
   -i ghcr.io/cyber-dojo-languages/clangpp_googlemock:806bfec \
-  sh -c "make kata_tests CXX=clang++ && ./kata_tests"
+  sh -c "make test"
